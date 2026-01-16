40d1d5c511f17f7f8faf77b84d4c3da7de601381
raelLacerdaDev <raellacerda.developer@gmail.com>
2026-01-16 10:28:10 -0300

    Add main function with multiple agualoo calls

---
+++ Main.kt
@@ -0,0 +1,40 @@
+fun main() {
+
+    agualoo(
+        n = 2,
+        m = 3,
+        diameters = intArrayOf(5, 4),
+        heights = intArrayOf(7, 8, 4)
+    )
+
+    agualoo(
+        n = 2, m = 1,
+        diameters = intArrayOf(5, 5),
+        heights = intArrayOf(10)
+    )
+
+    agualoo(
+        n = 2,
+        m = 4,
+        diameters = intArrayOf(7, 2),
+        heights = intArrayOf(4, 3, 1, 2)
+    )
+
+    agualoo(
+        n = 2,
+        m = 4,
+        diameters = intArrayOf(7, 2),
+        heights = intArrayOf(2, 1, 8, 5)
+    )
+
+    agualoo(
+        n = 2,
+        m = 10,
+        diameters = intArrayOf(1234567, 2345),
+        heights = intArrayOf(
+            12345610, 1, 123, 23564,
+            123456, 123, 2, 3, 2, 1
+        )
+    )
+    
+}
